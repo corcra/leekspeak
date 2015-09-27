d23f3b1bf3a26e22a7527515aa62d7760921fcd9
Stephanie Hyland <steph.hyland@gmail.com>
2015-09-27 17:40:08 -0400

    I'm on a plane so I can't check if there are any 'real' algorithms for achieving this task, so here's my improvised attempt at generating a mapping (i.e. an ordering of rows/columns) to 'maximise' the difference between two square matrices

--- get_map.py
+++ get_map.py
@@ -4,6 +4,7 @@
 from string import ascii_lowercase
 from itertools import product
 import numpy as np
+import pdb
 
 # --- loading and prep --- #
 def get_base32():
@@ -41,6 +42,72 @@ def get_language(path):
     W = len(vocabulary)
     print 'Loaded', W, 'words from', path
     return vocabulary, vectors
+
+# --- some optimisation stuff --- #
+def get_proposal(A, B):
+    """
+    Everyone loves MH.
+    """
+    n = A.shape[0]
+    # get a pivot point
+    inner_products = np.einsum('i...,...i', A, B)
+    violated = np.random.choice(n, size=1, p=inner_products/np.sum(inner_products))[0]
+    # get the rest
+    v = np.array([x for (i, x) in enumerate(A[:, violated]) if not i == violated])
+    phi = np.array([x for (i, x) in enumerate(B[:, violated]) if not i == violated])
+    # now reorder... (argsort gives low to high, remember)
+    v_order = np.argsort(v)
+    phi_order = np.argsort(-phi)
+    # want to move the highest phi to the lowest v
+    ordering_subset = v_order[phi_order]
+    ordering_subset[np.where(ordering_subset >= violated)] += 1
+    # reinsert into ordering
+    ordering = np.empty(shape=len(v)+1, dtype=np.int)
+    ordering[:violated] = ordering_subset[:violated]
+    ordering[violated] = violated
+    ordering[(violated+1):] = ordering_subset[violated:]
+    assert len(set(ordering)) == len(ordering)
+    return ordering
+
+def find_ordering(A, B, eps=0.00001):
+    """
+    Reorder the rows/columns of B to maximise its difference to A.
+    ... possibly.
+    Use Metropolis-Hastings for some reason.
+    """
+    assert A.shape[0] == A.shape[1]
+    assert B.shape == A.shape
+    diff = np.mean(abs(A - B))
+    temperature = diff
+    delta = 100
+    cumulative_delta = 0
+    print diff, delta
+    accept, reject = 0, 0
+    while abs(delta) > eps:
+        # get proposal ordering
+        proposal_ordering = get_proposal(A, B)
+        proposal_B = B[proposal_ordering, :][:, proposal_ordering]
+        proposal_diff = np.mean(abs(A - proposal_B))
+        # accept with some probability
+        proposal_delta = diff - proposal_diff
+        prob = min(1, np.exp(-proposal_delta/temperature))
+        if np.random.random() <= prob:
+            accept += 1
+            ordering = proposal_ordering
+            diff = proposal_diff
+            delta = proposal_delta
+            cumulative_delta -= delta
+            B = proposal_B
+            print diff, -delta, cumulative_delta
+            if accept%100 == 0:
+                temperature /= 1.1
+        else:
+            reject += 1
+            if reject%100 == 0:
+                temperature *= 1.15
+        temperature *= 0.99999
+    acceptance_rate = float(accept)/(accept + reject)
+    return ordering, acceptance_rate, temperature
 
 # --- maps --- #
 def random_map(triples, vocabulary):
